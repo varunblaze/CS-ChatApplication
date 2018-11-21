e7eb9a88e804b980d61622c5c08b687459b3ae5c
Varun Krishnamurthy <varun.blaze@gmail.com>
2018-11-21 13:51:53 +0530

    Delete start_app.sh

--- start_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x
-pwd
-unzip ./depends/ChatScriptNLP.zip
-chmod 755 ./ChatScriptNLP/ChatScriptNLP/BINARIES/LinuxChatScript64
-./ChatScriptNLP/ChatScriptNLP/BINARIES/LinuxChatScript64
-java -jar ./target/CSChatApplication-0.0.1-SNAPSHOT.jar 
