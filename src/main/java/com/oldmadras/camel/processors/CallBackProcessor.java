090bcb7da752fd6bdaaebe0dbd3c7bceb30b6e4c
Varun Krishnamurthy <varun.blaze@gmail.com>
2018-11-21 17:44:37 +0530

    Update CallBackProcessor.java

--- src/main/java/com/oldmadras/camel/processors/CallBackProcessor.java
+++ src/main/java/com/oldmadras/camel/processors/CallBackProcessor.java
@@ -1,12 +1,14 @@
 package com.oldmadras.camel.processors;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.chatscript.messages.ChatScriptMessage;
+
 
 public class CallBackProcessor implements Processor {
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		final String body = exchange.getIn().getBody(String.class);
+		final ChatScriptMessage body = exchange.getIn().getBody(ChatScriptMessage.class);
 		exchange.getOut().setBody(body);
 		exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 202);
 	}
