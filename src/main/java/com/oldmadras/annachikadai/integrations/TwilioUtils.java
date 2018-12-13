f0ddeec9394783bfd8265c015651df77f2b20dcd
varunblaze <varun.blaze@gmail.com>
2018-12-13 17:43:42 +0530

    chages

--- src/main/java/com/oldmadras/annachikadai/integrations/TwilioUtils.java
+++ src/main/java/com/oldmadras/annachikadai/integrations/TwilioUtils.java
@@ -11,14 +11,14 @@ public class TwilioUtils {
 	@Value("${TWILIO_AUTH_TOKEN}")
 	  public static  String AUTH_TOKEN;
 	@Value("${TWILIO_SOURCE_NUMBER}")
-	  public static  String SOURCE_NUMBER="+919962528868";
+	  public static  String SOURCE_NUMBER="+";
 	@Value("${TWILIO_TARGET_NUMBER}")
-	  public static  String TARGET_NUMBER="+14155238886";
+	  public static  String TARGET_NUMBER="+";
 	  private static boolean init=false;
 	
 	private static void initapi()
 	{
-		  Twilio.init("ACd253e825b3a8af79c2df9fff889f4fa2", "e3b9c5c5353f15b2e49063fefc598795");
+		  Twilio.init("", "");
 		  
 	}
 	  
